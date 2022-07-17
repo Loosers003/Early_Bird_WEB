422d7134590918635aee680c4fdd60b19677aa2d
saikumar41 <35288684+saikumar41@users.noreply.github.com>
2022-07-18 01:23:57 +0530

    Added Brand display pic upload

--- src/app/pages/data-management/create-brand/create-brand.component.ts
+++ src/app/pages/data-management/create-brand/create-brand.component.ts
@@ -12,12 +12,11 @@ import { DataManagementService } from '../data-management.service';
 })
 export class CreateBrandComponent implements OnInit {
   brand: BrandCreate
-  fileToUpload: any;
-  priorityList: Array<{value: number}>;
+  priorityList: Array<{ value: number }>;
   categoryList: Array<any>;
   showToaster: boolean;
   toast: { message: any; };
- 
+
 
   constructor(
     private dataManagementService: DataManagementService
@@ -25,8 +24,8 @@ export class CreateBrandComponent implements OnInit {
 
   ngOnInit(): void {
     this.brand = new BrandCreate();
-    this.priorityList = new Array<{value: number}>();
-    for(let i =1; i<=10;i++) {
+    this.priorityList = new Array<{ value: number }>();
+    for (let i = 1; i <= 10; i++) {
       const val = {
         value: i
       }
@@ -36,49 +35,39 @@ export class CreateBrandComponent implements OnInit {
   }
 
   getCategoryList() {
-    this.dataManagementService.getCategoryType('category_type').subscribe(response=>{
+    this.dataManagementService.getCategoryType('category_type').subscribe(response => {
       this.categoryList = response.reference_data;
     })
   }
 
   createBrand() {
-      this.dataManagementService.createBrand(this.brand).subscribe(response=>{
-      if(response.brand) {
+    this.dataManagementService.createBrand(this.brand).subscribe(response => {
+      if (response.brand) {
         this.brand = new BrandCreate();
       }
     },
-    error => {
-      if(error.status === 422) {
-        this.toast = {
-          message: error.error.message,
-        };
-        this.showToaster = true;
-      } else {
-        this.toast = {
-          message: error.message,
-        };
-        this.showToaster = true;
-      }
-    });
+      error => {
+        if (error.status === 422) {
+          this.toast = {
+            message: error.error.message,
+          };
+          this.showToaster = true;
+        } else {
+          this.toast = {
+            message: error.message,
+          };
+          this.showToaster = true;
+        }
+      });
   }
 
   handleFileInput(event) {
-   // this.brand.display_pic = window.URL.createObjectURL(event.srcElement.files[0]);
-   let reader;
-   let totalUrl;
-   if (event.target.files && event.target.files[0]) {
-     if (event.target.files[0].type === 'image/png' || event.target.files[0].type === 'image/jpg'
-       || event.target.files[0].type === 'image/jpeg') {
-       reader = new FileReader();
-       reader.readAsDataURL(event.target.files[0]); // read file as data url
-       reader.onload = (event) => { // called once readAsDataURL is completed
-         totalUrl = event.target.result;
-       //  this.brand.display_pic = totalUrl;
-       };
-     }
-   }
-   
-}
+    const file: File = event.target.files[0];
+    if (file) {
+      this.brand.display_pic = file
+    }
+
+  }
 
 
 
--- src/app/pages/data-management/data-management.service.ts
+++ src/app/pages/data-management/data-management.service.ts
@@ -3,36 +3,43 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { APIEndPoints } from 'src/app/features/common/apiEndPoints';
 import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators'; 
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class DataManagementService {
 
   constructor(
     private httpClient: HttpClient
   ) { }
 
-  getBrandsList(): Observable<any> { 
+  getBrandsList(): Observable<any> {
     return this.httpClient.get(`${environment.APP_API_BASE}${APIEndPoints.dataManagement.getAllBrands}`).pipe(map(response => {
-     return response;
+      return response;
     }))
   }
 
   getCategoryType(category): Observable<any> {
     return this.httpClient.get(`${environment.APP_API_BASE}${APIEndPoints.dataManagement.getCategoryType}?data_type=${category}`).pipe(map(response => {
       return response;
-     }))
-    
+    }))
+
   }
 
   createBrand(brand): Observable<any> {
-    console.log(brand.display_pic);
-  var formData: any = new FormData();
-  formData.append('name', brand.display_pic.name);
-  formData.append('brand', brand.display_pic.type);
-    return this.httpClient.post(`${environment.APP_API_BASE}${APIEndPoints.dataManagement.getAllBrands}`,formData, brand).pipe(map(response =>{
+    var formData: any = new FormData();
+    formData.append('brand[name]', brand.name);
+    formData.append('brand[priority]', brand.priority);
+    formData.append('brand[active]', true);
+    formData.append('brand[category_type]', brand.category_type);
+    formData.append('brand[description]', brand.comment);
+    formData.append('brand[display_pic]', brand.display_pic);
+    formData.append('brand[discount]', brand.discount);
+    console.log(formData)
+    return this.httpClient.post(`${environment.APP_API_BASE}${APIEndPoints.dataManagement.getAllBrands}`, formData).pipe(map(response => {
       return response;
     }))
   }
